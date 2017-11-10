a3687089e999894a509af85b0cfd214dd8b04968
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-10 21:01:00 +0100

    Add missing dependency

--- Sources/App/Infrastructure/Assembly/Assembly+Controller.swift
+++ Sources/App/Infrastructure/Assembly/Assembly+Controller.swift
@@ -2,7 +2,9 @@ import CoreService
 
 extension Assembly {
   var authRouteCollection: AuthRouteCollection {
-    return AuthRouteCollection()
+    return AuthRouteCollection(
+      authController: authController
+    )
   }
   
   private var authController: AuthController {
